74d931ae900897fdb32968a3930745bfe8d18578
bruggin9 <bruggin9@uwm.edu>
2021-12-15 13:54:25 -0600

    Update test_Course.py
    Updated the test_Course file to match the updated CourseManagement.py file to the best of my ability.

--- TAScheduler/tests/test_Course.py
+++ TAScheduler/tests/test_Course.py
@@ -16,21 +16,42 @@ class TestCreateCourse(TestCase):
     def setUp(self):
         self.instructor = UserProfile
         self.TA = UserProfile
-        CourseManagement.createCourse("Calculus", "12:00 PM", "M, W, F", "12:00 PM - 12:50 PM", self.instructor, self.TA)
+        CourseManagement.createCourse("001", "Calculus", "Physics 137", "M, W, F", "12:00 PM - 12:50 PM",
+                                      self.instructor, self.TA, "Section 800")
         self.testCourse = Course.objects.get(courseName="Calculus")
+
+    #     don't have the course location or a course ID when course is created.
+    # course time at 12:00pm is in the wrong spot, no lab is assigned to the course
+
+    def test_courseID(self):
+        self.assertEqual("001", self.testCourse.getCourseID(), "Course ID was not set correctly when creating course.")
+
+    def test_invalidCourseID(self):
+        with self.assertRaises(ValueError,
+                               msg="An exception was not raised when courseID passed invalid ID."):
+            CourseManagement.createCourse("001", "Calculus", "Physics 137", "M, W, F", "12:00 PM - 12:50 PM",
+                                          self.instructor, self.TA, "Section 800")
 
     def test_courseName(self):
         self.assertEqual("Calculus", self.testCourse.getCourseName(), "Course name was not set correctly when "
                                                                       "creating course.")
 
-    def test_courseTime(self):
-        self.assertEqual("12:00 PM", self.testCourse.getCourseTime(), "Course time was not set correctly when "
-                                                                      "creating course.")
+    def test_invalidCourseName(self):
+        with self.assertRaises(ValueError,
+                               msg="An exception was not raised when createCourse passed an invalid Course Name."):
+            CourseManagement.createCourse("001", "Calculus", "Physics 137", "M, W, F", "12:00 PM - 12:50 PM",
+                                          self.instructor, self.TA, "Section 800")
 
-    def test_invalidTime(self):
-        with self.assertRaises(ValueError, msg="An exception was not raised when createCourse was passed an invalid "
-                                               "time."):
-            CourseManagement.createCourse("Course", "50:00", "M, W, F", "12:00 PM - 12:50 PM")
+    def test_courseLocation(self):
+        self.assertEqual("Physics 137", self.testCourse.getCourseLocation(),
+                         "Course location was not set correctly when "
+                         "creating the course")
+
+    def test_invalidCourseLocation(self):
+        with self.assertRaises(ValueError,
+                               msg="An exception was not raised when createCourse passed an invalid Course Location."):
+            CourseManagement.createCourse("001", "Calculus", "Physics 137", "M, W, F", "12:00 PM - 12:50 PM",
+                                          self.instructor, self.TA, "Section 800")
 
     def test_courseDays(self):
         self.assertEqual("M, W, F", self.testCourse.getCourseDays(), "Course days were not set correctly when "
@@ -39,7 +60,8 @@ class TestCreateCourse(TestCase):
     def test_invalidDays(self):
         with self.assertRaises(ValueError,
                                msg="An exception was not raised when createCourse was passed invalid days."):
-            CourseManagement.createCourse("Course", "12:00", "invalid input", "12:00 PM - 12:50 PM")
+            CourseManagement.createCourse("001", "Calculus", "Physics 137", "M, W, F", "12:00 PM - 12:50 PM",
+                                          self.instructor, self.TA, "Section 800")
 
     def test_courseHours(self):
         self.assertEqual("12:00 PM - 12:50 PM", self.testCourse.getCourseHours(), "Course hours were not set "
@@ -48,16 +70,18 @@ class TestCreateCourse(TestCase):
     def test_invalidHours(self):
         with self.assertRaises(ValueError,
                                msg="An exception was not raised when createCourse was passed invalid hours."):
-            CourseManagement.createCourse("Course", "12:00", "M, W, F", "invalid")
+            CourseManagement.createCourse("001", "Calculus", "Physics 137", "M, W, F", "12:00 PM - 12:50 PM",
+                                          self.instructor, self.TA, "Section 800")
 
     def test_instructor(self):
         self.assertEqual(self.instructor, self.testCourse.getCourseInstructor(), "Course instructor was not set "
                                                                                  "correctly when creating course.")
 
     def test_invalidInstructor(self):
-        with self.assertRaises(TypeError, msg="An exception was not raised when createCourse was passed an invalid "
-                                              "instructor."):
-            CourseManagement.createCourse("Course", "12:00", "M, W, F", "12:00 PM - 12:50 PM", "This shouldn't be this")
+        with self.assertRaises(TypeError, msg="An exception was not raised when createCourse was passed an "
+                                              "invalid instructor"):
+            CourseManagement.createCourse("001", "Calculus", "Physics 137", "M, W, F", "12:00 PM - 12:50 PM",
+                                          self.instructor, self.TA, "Section 800")
 
     def test_TA(self):
         self.assertEqual(self.TA, self.testCourse.getCourseTa(),
@@ -66,8 +90,18 @@ class TestCreateCourse(TestCase):
     def test_invalidTA(self):
         with self.assertRaises(TypeError,
                                msg="An exception was not raised when createCourse was passed an invalid TA."):
-            CourseManagement.createCourse("Course", "12:00", "M, W, F", "12:00 PM - 12:50 PM", self.instructor,
-                                          "String")
+            CourseManagement.createCourse("001", "Calculus", "Physics 137", "M, W, F", "12:00 PM - 12:50 PM",
+                                          self.instructor, self.TA, "Section 800")
+
+    def test_Labs(self):
+        self.assertEqual(self.Labs, self.testCourse.getCourseLabs(),
+                         "Course Labs were not set correctly when creating course.")
+
+    def test_invalidLabs(self):
+        with self.assertRaises(TypeError,
+                               msg="An exception was not raised when createCourse was passed an invalid Lab."):
+            CourseManagement.createCourse("001", "Calculus", "Physics 137", "M, W, F", "12:00 PM - 12:50 PM",
+                                          self.instructor, self.TA, "Section 800")
 
 
 class TestEditCourse(TestCase):
@@ -78,31 +112,54 @@ class TestEditCourse(TestCase):
         self.TA = UserProfile
         self.TA2 = UserProfile
         # Course for invalid inputs
-        Course(courseName="Calculus", courseTime="12:00 PM", courseDays="M, W, F", courseInstructor=self.instructor,
-               courseTA=self.TA).save()
+        Course(courseID="001", courseName="Calculus", courseLocation="Physics 137", courseDays="M, W, F",
+               courseHours="12:00 PM - 12:50 PM",
+               courseInstructor=self.instructor, courseTA=self.TA, courseLabs="Section 800").save()
         # Course for editName
-        Course(courseName="Computer Programming", courseTime="1:00 PM", courseDays="T, Th",
-               courseInstructor=self.instructor2, courseTA=self.TA2).save()
+        Course(courseID="002", courseName="Computer Programming", courseLocation="Physics 135", courseDays="T, Th",
+               courseHours="1:00 PM - 1:50 PM",
+               courseInstructor=self.instructor2, courseTA=self.TA2, courseLabs="Section 801").save()
         # Course for everything else
-        Course(courseName="History", courseTime="5:00 PM", courseDays="M, T, W", courseInstructor=self.instructor,
-               courseTA=self.TA)
+        Course(courseID="003", courseName="History", courseLocation="Physics 133", courseDays="M, T, W",
+               courseHours="2:00 PM - 2:50 PM",
+               courseInstructor=self.instructor, courseTA=self.TA, courseLabs="Section 803").save()
 
-    def test_editName(self):
-        mathCourse = Course.objects.get(courseName="Computer Programming")
-        CourseManagement.editCourse(mathCourse, courseName="Trigonometry")
-        self.assertEqual("Trigonometry", mathCourse.courseName, "Name was not edited correctly.")
-
-    def test_editTime(self):
+    def test_editID(self):
         historyCourse = Course.objects.get(courseName="History")
-        CourseManagement.editCourse(historyCourse, courseTime="2:00 PM")
-        self.assertEqual("2:00 PM", historyCourse.courseTime, "Time was not edited correctly.")
+        CourseManagement.editCourse(historyCourse, courseID="003")
+        self.assertEqual("003", historyCourse.courseID, "ID was not edited correctly.")
 
-    def test_invalidTime(self):
+    def test_invalidID(self):
         mathCourse = Course.objects.get(courseName="Calculus")
         with self.assertRaises(ValueError,
-                               msg="An exception was not raises when editCourse was passed an invalid time."):
-            CourseManagement.editCourse(mathCourse, courseTime="25:00 PM")
-        self.assertEqual("12:00 PM", mathCourse.courseTime, "editCourse changed courseTime when it shouldn't have.")
+                               msg="An exception was not raises when editCourse was passed an invalid ID."):
+            CourseManagement.editCourse(mathCourse, courseID="Invalid")
+        self.assertEqual("001", mathCourse.courseID, "editCourse changed courseID when it shouldn't have.")
+
+    def test_editName(self):
+        computerCourse = Course.objects.get(courseName="Computer Programming")
+        CourseManagement.editCourse(computerCourse, courseName="Computer Programming")
+        self.assertEqual("Computer Programming", computerCourse.name, "Name was not edited correctly.")
+
+    def test_invalidName(self):
+        mathCourse = Course.objects.get(courseName="Calculus")
+        with self.assertRaises(ValueError,
+                               msg="An exception was not raises when editCourse was passed an invalid Name."):
+            CourseManagement.editCourse(mathCourse, courseName="Invalid")
+        self.assertEqual("Calculus", mathCourse.name, "editCourse changed courseName when it shouldn't have.")
+
+    def test_editLocation(self):
+        historyCourse = Course.objects.get(courseName="History")
+        CourseManagement.editCourse(historyCourse, courseLocation="Physics 101")
+        self.assertEqual("Physics 101", historyCourse.courseLocation, "Location was not edited correctly.")
+
+    def test_invalidLocation(self):
+        mathCourse = Course.objects.get(courseName="Calculus")
+        with self.assertRaises(ValueError,
+                               msg="An exception was not raises when editCourse was passed an invalid location."):
+            CourseManagement.editCourse(mathCourse, courseLocation="Invalid")
+        self.assertEqual("Physics 137", mathCourse.courseLocation, "editCourse changed courseLocation"
+                                                                   " when it shouldn't have.")
 
     def test_editDays(self):
         historyCourse = Course.objects.get(courseName="History")
@@ -114,6 +171,18 @@ class TestEditCourse(TestCase):
         with self.assertRaises(ValueError, msg="An exception was not raised when editCourse was passed bad days."):
             CourseManagement.editCourse(mathCourse, courseDays="Invalid")
         self.assertEqual("M, W, F", mathCourse.courseDays, "editCourse changed courseDays when they shouldn't have.")
+
+    def test_editHours(self):
+        historyCourse = Course.objects.get(courseName="History")
+        CourseManagement.editCourse(historyCourse, courseHours="3:00 - 3:50 PM")
+        self.assertEqual("3:00 - 3:50 PM", historyCourse.courseHours, "Hours were not edited correctly.")
+
+    def test_invalidHours(self):
+        mathCourse = Course.objects.get(courseName="Calculus")
+        with self.assertRaises(ValueError, msg="An exception was not raised when editCourse was passed bad hours."):
+            CourseManagement.editCourse(mathCourse, courseHours="Invalid")
+        self.assertEqual("12:00 PM - 12:50 PM", mathCourse.courseDays, "editCourse changed courseHours when they "
+                                                                       "shouldn't have.")
 
     def test_editInstructor(self):
         historyCourse = Course.objects.get(courseName="History")
@@ -141,14 +210,61 @@ class TestEditCourse(TestCase):
         self.assertEqual(self.TA, mathCourse.TA,
                          "editCourse changed TA when it shouldn't have.")
 
+    def test_editLabs(self):
+        historyCourse = Course.objects.get(courseName="History")
+        CourseManagement.editCourse(historyCourse, courseLabs="Section 804")
+        self.assertEqual("Section 805", historyCourse.courseTA, "Lab was not edited correctly.")
+
+    def test_invalidLabs(self):
+        mathCourse = Course.objects.get(courseName="Calculus")
+        with self.assertRaises(ValueError,
+                               msg="editCourse change courseLabs when they shouldn't have changed course Labs."):
+            CourseManagement.editCourse(mathCourse, courseLabs="Invalid")
+        self.assertEqual("Section 800", mathCourse.courseLabs,
+                         "editCourse changed Course Labs when it shouldn't have.")
+
 
 class TestDeleteCourse(TestCase):
     def setUp(self):
         self.instructor = UserProfile
         self.TA = UserProfile
-        Course(courseName="Art", courseTime="12:00 PM", courseDays="M, W, F", courseHours="12:00 PM - 12:50 PM", courseInstructor=self.instructor, courseTA=self.TA).save()
+        Course(courseName="Art", courseTime="12:00 PM", courseDays="M, W, F", courseHours="12:00 PM - 12:50 PM",
+               courseInstructor=self.instructor, courseTA=self.TA).save()
 
     def test_delete(self):
+        oldCourse = Course.objects.get(courseName="Art")
+        CourseManagement.deleteCourse(oldCourse)
+        self.assertEqual(None, oldCourse, "Course was not deleted successfully.")
+
+
+class TestPopulateSearch(TestCase):
+    def setUp(self):
+        self.instructor = UserProfile
+        self.TA = UserProfile
+
+    def test_populate_search(self):
+        oldCourse = Course.objects.get(courseName="Art")
+        CourseManagement.deleteCourse(oldCourse)
+        self.assertEqual(None, oldCourse, "Course was not deleted successfully.")
+
+
+class TestInputErrorChecker(TestCase):
+    def setUp(self):
+        self.instructor = UserProfile
+        self.TA = UserProfile
+
+    def test_input_error_checker(self):
+        oldCourse = Course.objects.get(courseName="Art")
+        CourseManagement.deleteCourse(oldCourse)
+        self.assertEqual(None, oldCourse, "Course was not deleted successfully.")
+
+
+class TestDisplayCourses(TestCase):
+    def setUp(self):
+        self.instructor = UserProfile
+        self.TA = UserProfile
+
+    def test_display_courses(self):
         oldCourse = Course.objects.get(courseName="Art")
         CourseManagement.deleteCourse(oldCourse)
         self.assertEqual(None, oldCourse, "Course was not deleted successfully.")
