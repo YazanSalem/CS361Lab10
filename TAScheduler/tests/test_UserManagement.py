99087a4b37cd957e12d52fb6bd8b5394ad8c13e3
bruggin9 <bruggin9@uwm.edu>
2021-12-19 14:03:14 -0600

    Update test_UserManagement.py
    Added editUser and deleteUser tests

--- TAScheduler/tests/test_UserManagement.py
+++ TAScheduler/tests/test_UserManagement.py
@@ -36,18 +36,6 @@ class TestMyUser(TestCase):
         # self.testCourse = Course.objects.get(courseID=1000)
         # self.testCourse = Course.objects.get(courseID=1000)
 
-    def test_invalid_login(self):
-        response = self.client.post('/', {'username': 'Superman', 'password': '24680'})
-        self.assertEqual(response.url, 'invalid_login_response')
-        # print(response.context['error_messages'])
-        self.assertEqual(response.context['error'], 'username or password are incorrect')
-
-    def test_valid_login(self):
-        response = self.client.post('/', {'username': 'Samuel', 'password': '12345'})
-        self.assertEqual(response.url, '/valid_login_response/')
-        # print(response.context['error_messages'])
-        self.assertEqual(response.context['Validation'], 'username or password are correct')
-
     def test_UserID(self):
         self.assertEqual(1000, self.testUser.userID, "User ID was not set correctly when creating a User.")
 
@@ -75,7 +63,7 @@ class TestMyUser(TestCase):
 
     def test_UserUsername(self):
         self.assertEqual("mrwick123", self.testUser.username, "User username was not set correctly when "
-                                                                    "creating a User.")
+                                                              "creating a User.")
 
     def test_InvalidUsername(self):
         # self.assertEqual("not johnwick", self.testUser.UserUsername, "User username was not set correctly when "
@@ -90,7 +78,7 @@ class TestMyUser(TestCase):
 
     def test_UserPassword(self):
         self.assertEqual("password", self.testUser.password, "User Password was not set correctly when creating a "
-                                                                 "User.")
+                                                             "User.")
 
     def test_InvalidUserPassword(self):
         # self.assertEqual("incorrectPassword", self.testUser.UserPassword, "User Password was not set correctly when "
@@ -119,9 +107,9 @@ class TestMyUser(TestCase):
 
     def test_UserAddress(self):
         self.assertEqual("894 Lake Street, Milwaukee, Wisconsin 99999", self.testUser.address, "User Address was "
-                                                                                                   "not set correctly"
-                                                                                                   " when creating a "
-                                                                                                   "User.")
+                                                                                               "not set correctly"
+                                                                                               " when creating a "
+                                                                                               "User.")
 
     def test_InvalidUserAddress(self):
         # self.assertEqual("123 Kenwood Ave, Milwaukee, Wisconsin 53211", self.testUser.UserAddress, "User Address was "
@@ -151,7 +139,7 @@ class TestMyUser(TestCase):
 
     def test_UserEmail(self):
         self.assertEqual("johnwick123@uwm.edu", self.testUser.email, "User Email was not set correctly when "
-                                                                         "creating a User.")
+                                                                     "creating a User.")
 
     def test_InvalidUserEmail(self):
         # self.assertEqual("notjohnwick123@uwm.edu", self.testUser.UserEmail, "User Email was not set correctly when "
@@ -163,3 +151,127 @@ class TestMyUser(TestCase):
                                       name="John Doe",
                                       address="3400 N Maryland Ave, Milwaukee, Wisconsin 53211",
                                       phone=4142340987, email=123456)
+
+    def test_EditUser(self):
+        UserManagement.editUser(user_id=1000)
+        self.assertTrue(UserProfile.objects.filter(userID=1000).exists())
+
+    def test_editInvalidUser(self):
+        with self.assertRaises(ValueError, msg="An exception was not raised when edit was passed a user_id that "
+                                               "does not exist"):
+            UserManagement.editUser(user_id=1000)
+
+    def test_deleteUser(self):
+        UserManagement.deleteUser(user_id=1000)
+        self.assertFalse(UserProfile.objects.filter(userID=1000).exists())
+
+    def test_deleteInvalidUser(self):
+        with self.assertRaises(ValueError, msg="An exception was not raised when delete was passed a user_id that "
+                                               "does not exist"):
+            UserManagement.deleteUser(user_id=1000)
+
+
+class TestEditUser(TestCase):
+    def setUp(self):
+        testusr0 = UserProfile.objects.create()
+        testusr1 = UserManagement.createUser(user_id=1000, user_type="SUPERVISOR", username="mrwick123",
+                                             password="password",
+                                             name="John Wick", address="894 Lake Street, Milwaukee, Wisconsin 99999",
+                                             phone=4142542688,
+                                             email="johnwick123@uwm.edu")
+        self.supervisor = UserProfile.objects.get(userID=1000)
+        testusr2 = UserManagement.createUser(user_id=1001, user_type="INSTRUCTOR", username="mrbond123",
+                                             password="password",
+                                             name="James Bond", address="123 Kenwood Ave, Milwaukee, Wisconsin 99999",
+                                             phone=4144567890,
+                                             email="jamesbond123@uwm.edu")
+        self.instructor = UserProfile.objects.get(userID=1001)
+        testusr3 = UserManagement.createUser(user_id=1002, user_type="TA", username="willferrell123",
+                                             password="password",
+                                             name="Will Ferrell",
+                                             address="123 Hollywood Blvd, Madison, Wisconsin 53207",
+                                             phone=4140394122,
+                                             email="willferrell123@uwm.edu")
+        self.TA = UserProfile.objects.get(userID=1002)
+        self.testUser = UserProfile.objects.get(userID=1000, userType="SUPERVISOR", username="mrwick123",
+                                                password="password",
+                                                name="John Wick", address="894 Lake Street, Milwaukee, Wisconsin 99999",
+                                                phone=4142542688,
+                                                email="johnwick123@uwm.edu")
+
+    def test_editUserID(self):
+        UserManagement.editUser(user_id=1002)
+        self.assertEqual(1002, UserProfile.objects.get(userID=1002).userID, "UserID was not edited correctly.")
+
+    # def test_invalidID(self):
+    #     with self.assertRaises(TypeError,
+    #                            msg="editUser does not raise an error when passed a userID that doesn't correspond "
+    #                                "to a user"):
+    #         UserManagement.editUser(userID=1000, userType="SUPERVISOR", username="mrwick123",
+    #                                 password="password",
+    #                                 name="John Wick", address="894 Lake Street, Milwaukee, Wisconsin 99999",
+    #                                 phone=4142542688,
+    #                                 email="johnwick123@uwm.edu")
+
+    def test_editUserType(self):
+        UserManagement.editUser(user_id=1002, user_type="TA")
+        self.assertEqual("TA", UserProfile.objects.get(userID=1002).userType, "User Type was not edited correctly.")
+
+    # def test_editInvalidUserType(self):
+    #     with self.assertRaises(TypeError,
+    #                            msg="editUser does not raise an error when passed a userID that doesn't correspond "
+    #                                "to a user"):
+    #         UserManagement.editUser(userID=1000, userType="SUPERVISOR", username="mrwick123",
+    #                                 password="password",
+    #                                 name="John Wick", address="894 Lake Street, Milwaukee, Wisconsin 99999",
+    #                                 phone=4142542688,
+    #                                 email="johnwick123@uwm.edu")
+
+    def test_editUsername(self):
+        UserManagement.editUser(user_id=1002, username="willferrell123")
+        self.assertEqual("willferrell123", UserProfile.objects.get(userID=1002).username,
+                         "Username was not edited correctly.")
+
+    def test_editPassword(self):
+        UserManagement.editUser(user_id=1002, password="password")
+        self.assertEqual("password", UserProfile.objects.get(userID=1002).password,
+                         "Passowrd was not edited correctly.")
+
+    def test_editName(self):
+        UserManagement.editUser(user_id=1002, name="John Wick")
+        self.assertEqual("John Wick", UserProfile.objects.get(userID=1002).name, "Name was not edited correctly.")
+
+    def test_editAddress(self):
+        UserManagement.editUser(user_id=1000, address="894 Lake Street, Milwaukee, Wisconsin 99999")
+        self.assertEqual("894 Lake Street, Milwaukee, Wisconsin 99999", UserProfile.objects.get(userID=1000).address,
+                         "Location was not edited correctly.")
+
+    def test_editPhone(self):
+        UserManagement.editUser(user_id=1002, phone=4140394122)
+        self.assertEqual(4140394122, UserProfile.objects.get(userID=1002).phone,
+                         "Phone was not edited correctly.")
+
+    def test_editEmail(self):
+        UserManagement.editUser(user_id=1002, email="willferrell123@uwm.edu")
+        self.assertEqual("willferrell123@uwm.edu", UserProfile.objects.get(userID=1002).email,
+                         "Email was not edited correctly.")
+
+
+class TestDeleteUser(TestCase):
+    def setUp(self):
+        UserManagement.createUser(user_id=1, user_type="INSTRUCTOR", username="instructor", password="password",
+                                  name="Professor Layton", address="3400 N Maryland Ave", phone="6085555432",
+                                  email="layton@gmail.com")
+        self.instructor = UserProfile.objects.get(userID=1)
+        UserProfile.objects.create(userID=10, userType="SUPERVISOR", username="WillFerrell", password="comedy",
+                                   name="Will Ferrell", address="3400 N Maryland Ave", phone="6085555432",
+                                   email="layton@gmail.com")
+
+    def test_delete(self):
+        UserManagement.deleteUser(user_id=1)
+        self.assertFalse(UserProfile.objects.filter(userID=1).exists())
+
+    def test_deleteInvalid(self):
+        with self.assertRaises(ValueError, msg="An exception was not raised when delete was passed a user_id that "
+                                               "does not exist"):
+            UserManagement.deleteUser(user_id=1)
