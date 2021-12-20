4f36ae711c5a6a068fd19c015ba069b550a2014c
WajahatAliShah1 <walishah@uwm.edu>
2021-12-19 19:06:28 -0600

    Update test_DeleteLab.py
    Formatted + Fixed Typo. There are still bugs with tests.

--- TAScheduler/tests/test_DeleteLab.py
+++ TAScheduler/tests/test_DeleteLab.py
@@ -1,10 +1,10 @@
 from django.test import TestCase, Client
 from TAScheduler.models import *
 
+
 # Delete Lab Cases - Successful Delete (No Unsuccessful Delete Cases)
 class DeleteLab(TestCase):
-    
-    dummyClient= None
+    dummyClient = None
     TA = None
     instructor = None
     admin = None
@@ -13,25 +13,29 @@ class DeleteLab(TestCase):
 
     def setUp(self):
         self.dummyClient = Client()
-        self.TA = UserProfile.objects.create(userID = 1, userTyoe = "TA", username = "TA1"
-        , password = "TA123", name = "TA Dummy", address = "TA Address", phone = 3234457876, email ="TAEmail@email.com")
-        
-        self.instructor = UserProfile.objects.create(userID = 2, userTyoe = "INSTRUCTOR", username = "Instructor1"
-        , password = "Instructor123", name = "Instructor Dummy", address = "Instructor Address", phone = 3234457176, email ="InstructorEmail@email.com")
+        self.TA = UserProfile.objects.create(userID=1, userType="TA", username="TA1"
+                                             , password="TA123", name="TA Dummy", address="TA Address",
+                                             phone=3234457876, email="TAEmail@email.com")
 
-        self.admin = UserProfile.objects.create(userID = 3, userTyoe = "SUPERVISOR", username = "Admin1"
-        , password = "Admin123", name = "Admin Dummy", address = "Admin Address", phone = 3234452176, email ="AdminEmail@email.com")
+        self.instructor = UserProfile.objects.create(userID=2, userType="INSTRUCTOR", username="Instructor1"
+                                                     , password="Instructor123", name="Instructor Dummy",
+                                                     address="Instructor Address", phone=3234457176,
+                                                     email="InstructorEmail@email.com")
 
-        self.course = Course.objects.create(courseID = 1, name = "Software Engineering",
-        location="EMS 180", hours = "12:00PM - 01:00PM", days ="M, W", instructor = self.instructor,
-         TAs = [self.TA])
+        self.admin = UserProfile.objects.create(userID=3, userType="SUPERVISOR", username="Admin1"
+                                                , password="Admin123", name="Admin Dummy", address="Admin Address",
+                                                phone=3234452176, email="AdminEmail@email.com")
 
-        self.lab = Lab.objects.create(labID = 1, name = "Lab", location ="EMS 280",
-            hours = "03:00PM - 04:00PM", days="M, W", course = self.course, TA = self.TA)
+        self.course = Course.objects.create(courseID=1, name="Software Engineering",
+                                            location="EMS 180", hours="12:00PM - 01:00PM", days="M, W",
+                                            instructor=self.instructor)
+        self.course.TAs.add(self.TA)
+        self.lab = Lab.objects.create(labID=1, name="Lab", location="EMS 280",
+                                      hours="03:00PM - 04:00PM", days="M, W", course=self.course, TA=self.TA)
 
     def test_deleteLab(self):
-        resp = self.dummyClient.post('/delete_lab/', {'labID' : 1}, follow= True)
+        resp = self.dummyClient.post('/delete_lab/', {'delete': self.lab.labID}, follow=True)
         var = Lab.objects.count()
         self.assertEquals(var, 0)
-        allLabs = list(Lab.objects.filter(labID = 1))
+        allLabs = list(Lab.objects.filter(labID=1))
         self.assertEquals([], allLabs)
