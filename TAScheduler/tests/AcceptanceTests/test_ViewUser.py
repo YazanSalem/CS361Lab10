f5e642e67b40ad07d58b001c62541bace6992e73
BMHebert <70716012+BMHebert@users.noreply.github.com>
2021-12-20 00:16:42 -0600

    Update test_ViewUser.py
    Wrote setup methods

--- TAScheduler/tests/AcceptanceTests/test_ViewUser.py
+++ TAScheduler/tests/AcceptanceTests/test_ViewUser.py
@@ -2,19 +2,24 @@ from django.test import TestCase, Client
 from TAScheduler.models import UserProfile
 
 
-class TestViewSupervisorUsers(TestCase):
+class TestViewUsersSupervisor(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user1 = UserProfile.objects.create(name='Samuel', password='Samuel12345')
-        self.user2 = UserProfile.objects.create(name='John', password='John12345')
-        self.supervisor = UserProfile.objects.create(userID=1, userType="SUPERVISOR", username="supervisor", password="password")
+        self.user1 = UserProfile.objects.create(userID=1, userType="INSTRUCTOR", username="Samuel",
+                                                password="Samuel12345", name='Samuel', address="Address1",
+                                                phone=9876543212, email="sam@uwm.edu")
+        self.user2 = UserProfile.objects.create(userID=2, userType="TA", username="John", password="John12345",
+                                                name="John", address="Address2", phone=1234567890, email="john@uwm.edu")
+        self.supervisor = UserProfile.objects.create(userID=3, userType="SUPERVISOR", username="supervisor",
+                                                     password="password", name="SupervisorGuy",
+                                                     address="123 Street Street", phone=3211234567,
+                                                     email="supervisor@supervisor.gov")
         self.client.post("/", {"useraccount": self.supervisor.username, "password": self.supervisor.password})
         self.resp = self.client.get("/view_users/")
 
-
     def test_userID(self):
         for user in self.resp.context["user_list"]:
-
+            self.assertIn(str(user.userID), str(self.resp.content))
 
     def test_userType(self):
         pass
@@ -40,17 +45,14 @@ class TestViewSupervisorUsers(TestCase):
     def test_email(self):
         pass
 
+
 class TestViewUsersInstructor(TestCase):
     def setUp(self):
-        self.client = Client()
-        self.user1 = UserProfile.objects.create(name='Samuel', password='Samuel12345')
-        self.user2 = UserProfile.objects.create(name='John', password='John12345')
-        self.supervisor = UserProfile.objects.create(userID=1, userType="INSTRUCTOR", username="instructor", password="password")
-        self.client.post("/", {"useraccount": self.supervisor.username, "password": self.supervisor.password})
-        self.resp = self.client.get("/view_users/")
+        TestViewUsersSupervisor.setUp(self)
 
     def test_userID(self):
-        for user in self.resp.context["user_list"]:
+        # for user in self.resp.context["user_list"]:
+        pass
 
     def test_userType(self):
         pass
