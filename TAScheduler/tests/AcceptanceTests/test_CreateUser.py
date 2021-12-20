14b92d514932ef1686fd79791f0a4923c84a252c
WajahatAliShah1 <walishah@uwm.edu>
2021-12-19 19:18:01 -0600

    Update test_CreateUser.py

--- TAScheduler/tests/AcceptanceTests/test_CreateUser.py
+++ TAScheduler/tests/AcceptanceTests/test_CreateUser.py
@@ -9,16 +9,17 @@ class TestCreate(TestCase):
                                                 password="admin", name="Admin", address="address",
                                                 phone=1234567890, email="a@a.com")
         self.client = Client()
-        print(self.testSupervisor)
-        r = self.client.post("/create_user/", {"edit": self.userToEdit.userID}, follow=True)
+        self.createUser = None
+        print(self.admin)
+        r = self.client.post("/create_user/", follow=True)
 
     def test_create(self):
-        self.client.post("/create_user/",
-                         {"user_id": 2, "user_type": "SUPERVISOR", "username": "supervisorTest",
-                          "password": "supervisorTest",
-                          "name": "Test Supervisor", "address": "address",
-                          "phone": 1234567891,
-                          "email": "s@s.com"}, follow=True)
+        r = self.client.post("/create_user/",
+                             {"user_id": 2, "user_type": "SUPERVISOR", "username": "supervisorTest",
+                              "password": "supervisorTest",
+                              "name": "Test Supervisor", "address": "address",
+                              "phone": 1234567891,
+                              "email": "s@s.com"}, follow=True)
 
         self.assertEqual(UserProfile.objects.get(userID=2).userType, "INSTRUCTOR",
                          "Edit User did not set userType correctly")
